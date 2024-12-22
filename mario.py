f0cc2a20a7e543f1be9e881a41c5bb768c174929
BjoernSchilberg <bjoern.schilberg@posteo.de>
2024-12-22 21:15:02 +0100

    Added 2 alternatives to process PIXELS

--- mario.py
+++ mario.py
@@ -42,4 +42,22 @@ for y, row in enumerate(PIXELS):
         color = COLORS.get(pixel, 0)  # Default color is black
         pyxel.pset(x, y, color)
 
+# Alternativ 1
+# for y in range(len(PIXELS)):
+#    row = PIXELS[y]
+#    for x in range(len(row)):
+#        pixel = row[x]
+#        color = COLORS.get(pixel, 0)  # Default color is black
+#        pyxel.pset(x, y, color)
+
+# Alternativ 2
+# y = 0  # Initial y-coordinate
+# for row in PIXELS:
+#    x = 0  # Initial x-coordinate for each row
+#    for pixel in row:
+#        color = COLORS.get(pixel, 0)  # Default color is black
+#        pyxel.pset(x, y, color)
+#        x += 1  # Increment x-coordinate
+#    y += 1  # Increment y-coordinate
+
 pyxel.show()
